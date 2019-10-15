77ab9895ba71980732984ea179c8973aa9f47ba4
unknown <moonkiara42@gmail.com>
2019-10-15 23:56:27 +0600

    Added

---
+++ VechicleAppPractice2/VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    class Vehicle
+    {
+        public string Name;
+        public string RegNo;
+        public List<double> speeds = new List<double> { };
+
+        public Vehicle(string vehicleName, string regNo)
+        {
+            this.Name = vehicleName;
+            this.RegNo = regNo;
+
+        }
+
+
+        public double SetSpeed(double speed)
+        {
+            speeds.Add(speed);
+            return speed;
+        }
+
+        public double MaxSpeed()
+        {
+            double MaxSpeed = speeds.Max();
+            return MaxSpeed;
+
+        }
+
+
+        public double MinSpeed()
+        {
+            double MinSpeed = speeds.Min();
+            return MinSpeed;
+
+        }
+
+        public double AvgSpeed()
+        {
+            double AvgSpeed = speeds.Average();
+            return AvgSpeed;
+        }
+
+    }
+}
---
+++ VechicleAppPractice2/VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        Vehicle vehicle;
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            //string name = vehicleNameTextBox.Text;
+            //string regNo = regNoTextBox.Text;
+            //string speed = speedTextBox.Text;
+
+            vehicle = new Vehicle(vehicleNameTextBox.Text, regNoTextBox.Text);
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+
+
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            vehicle.SetSpeed(Convert.ToDouble(speedTextBox.Text));
+            speedTextBox.Clear();
+
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = vehicle.MinSpeed().ToString();
+            maxSpeedTextBox.Text = vehicle.MaxSpeed().ToString();
+            averageSpeedTextBox.Text = vehicle.AvgSpeed().ToString();
+
+
+
+
+        }
+    }
+}
